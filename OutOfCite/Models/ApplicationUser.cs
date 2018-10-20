e7a00447e1f6056c5fcc04b7093501cf2bc1089c
awieckert <wiec1369@gmail.com>
2018-10-20 15:04:05 -0500

    Working on the initial set up. Created custome applicationUser and register form. Still need to go through the StartUp.cs and finish all the Models/table relationships

---
+++ OutOfCite/Models/ApplicationUser.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OutOfCite.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser()
+        {
+
+        }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [NotMapped]
+        public string FullName => $"{FirstName} {LastName}";
+
+        [Required]
+        public string LinkedIn { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+
+        public virtual ICollection<PaymentType> PaymentTypes { get; set; }
+    }
+}
