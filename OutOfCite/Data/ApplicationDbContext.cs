b6ee85eb009f889be568c89dff40def326d8fd65
awieckert <wiec1369@gmail.com>
2018-10-21 21:27:21 -0500

    Finished the Toxicology authors and articles seed

--- OutOfCite/Data/ApplicationDbContext.cs
+++ OutOfCite/Data/ApplicationDbContext.cs
@@ -106,7 +106,7 @@ namespace OutOfCite.Data
                 },
                 new Article()
                 {
-                    Id = 2,
+                    Id = 2, 
                     AuthorId = 1,
                     AffiliationId = 1,
                     Title = "Thinking about brain and consciousness",
@@ -140,6 +140,42 @@ namespace OutOfCite.Data
                     Journal = "Journal of Neuroinflammation",
                     JournalImpact = 4,
                     Citations = 1
+                },
+                new Article ()
+                {
+                    Id = 5,
+                    AuthorId = 5,
+                    AffiliationId = 2,
+                    Title = "Crizotinib-induced abnormal signal processing in the retina",
+                    Abstract = "Molecular target therapy for cancer is characterized by unique adverse effects that are not usually observed with cytotoxic chemotherapy. For example, the anaplastic lymphoma kinase (ALK)-tyrosine kinase inhibitor crizotinib causes characteristic visual disturbances, whereas such effects are rare when another ALK-tyrosine kinase inhibitor, alectinib, is used. To elucidate the mechanism responsible for these visual disturbances, the responses to light exhibited by retinal ganglion cells treated with these agents were evaluated using a C57BL6 mouse ex vivo model. Both crizotinib and alectinib changed the firing rate of ON and OFF type retinal ganglion cells. However, the ratio of alectinib-affected cells (15.7%) was significantly lower than that of crizotinib-affected cells (38.6%). Furthermore, these drugs changed the response properties to light stimuli of retinal ganglion cells in some of the affected cells, i.e., OFF cells responded to both ON and OFF stimuli, etc. Finally, the expressions of ALK (a target receptor of both crizotinib and alectinib) and of MET and ROS1 (additional target receptors of crizotinib) were observed at the mRNA level in the retina. Our findings suggest that these drugs might target retinal ganglion cells and that the potency of the drug actions on the light responses of retinal ganglion cells might be responsible for the difference in the frequencies of visual disturbances observed between patients treated with crizotinib and those treated with alectinib. The present experimental system might be useful for screening new molecular target agents prior to their use in clinical trials.",
+                    URL = "https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0135521",
+                    Journal = "PLoS One",
+                    JournalImpact = 2.7,
+                    Citations = 3
+                },
+                new Article ()
+                {
+                    Id = 6,
+                    AuthorId = 6,
+                    AffiliationId = 2,
+                    Title = "In vitro evolution and affinity-maturation with Coliphage Qβ display",
+                    Abstract = "The Escherichia coli bacteriophage, Qβ (Coliphage Qβ), offers a favorable alternative to M13 for in vitro evolution of displayed peptides and proteins due to high mutagenesis rates in Qβ RNA replication that better simulate the affinity maturation processes of the immune response. We describe a benchtop in vitro evolution system using Qβ display of the VP1 G-H loop peptide of foot-and-mouth disease virus (FMDV). DNA encoding the G-H loop was fused to the A1 minor coat protein of Qβ resulting in a replication-competent hybrid phage that efficiently displayed the FMDV peptide. The surface-localized FMDV VP1 G-H loop cross-reacted with the anti-FMDV monoclonal antibody (mAb) SD6 and was found to decorate the corners of the Qβ icosahedral shell by electron microscopy. Evolution of Qβ-displayed peptides, starting from fully degenerate coding sequences corresponding to the immunodominant region of VP1, allowed rapid in vitro affinity maturation to SD6 mAb. Qβ selected under evolutionary pressure revealed a non-canonical, but essential epitope for mAb SD6 recognition consisting of an Arg-Gly tandem pair. Finally, the selected hybrid phages induced polyclonal antibodies in guinea pigs with good affinity to both FMDV and hybrid Qβ-G-H loop, validating the requirement of the tandem pair epitope. Qβ-display emerges as a novel framework for rapid in vitro evolution with affinity-maturation to molecular targets.",
+                    URL = "https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0113069",
+                    Journal = "PLoS One",
+                    JournalImpact = 2.7,
+                    Citations = 2
+                },
+                new Article ()
+                {
+                    Id = 7,
+                    AuthorId = 7,
+                    AffiliationId = 2,
+                    Title = "High-Throughput RNA Interference Screening: Tricks of the Trade",
+                    Abstract = "The process of validating an assay for high-throughput screening (HTS) involves identifying sources of variability and developing procedures that minimize the variability at each step in the protocol. The goal is to produce a robust and reproducible assay with good metrics. In all good cell-based assays, this means coefficient of variation (CV) values of less than 10% and a signal window of fivefold or greater. HTS assays are usually evaluated using Z′ factor, which incorporates both standard deviation and signal window. A Z′ factor value of 0.5 or higher is acceptable for HTS. We used a standard HTS validation procedure in developing small interfering RNA (siRNA) screening technology at the HTS center at Southern Research. Initially, our assay performance was similar to published screens, with CV values greater than 10% and Z′ factor values of 0.51 ± 0.16 (average ± standard deviation). After optimizing the siRNA assay, we got CV values averaging 7.2% and a robust Z′ factor value of 0.78 ± 0.06 (average ± standard deviation). We present an overview of the problems encountered in developing this whole-genome siRNA screening program at Southern Research and how equipment optimization led to improved data quality.",
+                    URL = "http://journals.sagepub.com/doi/abs/10.1177/2211068213486786",
+                    Journal = "	Journal of Laboratory Automation",
+                    JournalImpact = 2.6,
+                    Citations = 4
                 }
                 );
 
@@ -171,6 +207,34 @@ namespace OutOfCite.Data
                     FirstName = "David",
                     LastName = "Standard",
                     HIndex = 66
+                },
+                new Author()
+                {
+                    Id = 5,
+                    FirstName = "Steven",
+                    LastName = "Barnes",
+                    HIndex = 30
+                },
+                new Author()
+                {
+                    Id = 6,
+                    FirstName = "Steven",
+                    LastName = "Aller",
+                    HIndex = 29
+                },
+                new Author()
+                {
+                    Id = 7,
+                    FirstName = "Mary Ann",
+                    LastName = "Bjornsti",
+                    HIndex = 30
+                },
+                new Author()
+                {
+                    Id = 8,
+                    FirstName = "",
+                    LastName = "",
+                    HIndex = 3
                 }
                 );
         }
